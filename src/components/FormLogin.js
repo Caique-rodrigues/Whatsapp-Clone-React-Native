76cc8f7eea3564c7fe828235d26d353bac129ce4
Caique-rodrigues <caique_r.s@hotmail.com>
2018-06-06 00:48:30 -0300

    página de cadastro criada

---
+++ src/components/FormLogin.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native'
+
+
+export default class FormLogin extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.mainView}>
+
+                <View style={styles.firstView} >
+                    <Text style={styles.mainText}>Whatsapp Clone</Text>
+                </View>
+
+                <View style={styles.secondView} >
+                    <TextInput placeholder='E-mail' style={styles.textInputStyle1} />
+                    <TextInput placeholder='Senha' style={styles.textInputStyle1} />
+                    <Text style={styles.textSignIn}>Ainda não tem cadastro?  Cadastre-se</Text>
+                </View>
+
+                <View style={styles.thirdView} >
+                    <TouchableOpacity onPress={() => false} style={styles.buttonAccess}>
+                        <Text style={styles.textButtonStyle}>ACESSAR</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
+        )
+
+    }
+
+}
+
+const styles = StyleSheet.create({
+    mainText: {
+        fontSize: 25
+    },
+    mainView: {
+        flex: 1,
+        padding: 10,
+    },
+    firstView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    secondView: {
+        flex: 2
+    },
+    thirdView: {
+        flex: 2
+    },
+    textInputStyle1: {
+        fontSize: 20,
+        height: 45
+    },
+    textSignIn: {
+        fontSize: 20
+    },
+    buttonAccess: {
+        backgroundColor: '#115e54',
+        height: 35,
+        borderRadius: 2,
+        alignItems: 'center',
+        padding: 10
+    },
+    textButtonStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+
+
+
+})
